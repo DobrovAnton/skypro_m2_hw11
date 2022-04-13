1ca11abf4686d017d90ce126df325279ae184827
DobrovAnton <antdobrov@gmail.ru>
2022-04-13 11:36:19 +0700

    job completion

--- main.py
+++ main.py
@@ -4,12 +4,37 @@
 """
 
 # Импорт Flask и функций.
-from flask import Flask
+from flask import Flask, render_template
 import utils
 
 app = Flask(__name__)
 
+candidates_data = utils.load_candidates_from_json('candidates.json')
 
- # Запуск веб-приложения.
-if __name__ == "__main__":
-    app.run(debug=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html', candidates=candidates_data)
+
+
+@app.route('/candidate/<int:uid>')
+def profile(uid):
+    candidate = utils.get_candidate(uid)
+    return render_template('profile.html', candidate=candidate)
+
+
+@app.route('/search/<candidate_name>')
+def search(candidate_name):
+    candidates = utils.get_candidates_by_name(candidate_name)
+    return render_template('search.html', candidates=candidates, candidates_number=len(candidates))
+
+
+@app.route('/skills/<skill>')
+def get_skills(skill):
+    candidates = utils.get_candidates_by_skill(skill)
+    return render_template('skills.html', candidates=candidates, candidates_number=len(candidates), skill=skill)
+
+
+# Запуск веб-приложения.
+if __name__ == '__main__':
+    app.run(debug=False)
--- utils.py
+++ utils.py
@@ -5,47 +5,59 @@
 
 import json
 
+# База данных кандидатов.
+__candidates_data = []
+
 
 def load_candidates_from_json(path):
     """
-
+    Загружает базу данных по кандидатам из json файла
+    и преобразует в словарь.
     :param path:
-    :return:
+    :return: обработанная база данных кандидатов.
     """
-    pass
+    global __candidates_data
+    with open(path, 'r', encoding='utf-8') as file:
+        __candidates_data = json.load(file)
+        return __candidates_data
 
 
 def get_candidate(candidate_id):
     """
-
-    :param candidate_id:
-    :return:
+    Загружает данные о кандидате по указанному id.
+    :param candidate_id: id, по которому необходимо осуществить поиск.
+    :return: данные о кандидате.
     """
-    pass
-
-
-def get_candidate(candidate_id):
-    """
-
-    :param candidate_id:
-    :return:
-    """
-    pass
+    for candidate in __candidates_data:
+        if candidate['id'] == candidate_id:
+            return {
+                'name': candidate['name'],
+                'position': candidate['position'],
+                'picture': candidate['picture'],
+                'skills': candidate['skills'],
+            }
+    return {'not_found': 'Кандидат не найден'}
 
 
 def get_candidates_by_name(candidate_name):
     """
-
-    :param candidate_name:
-    :return:
+    Загружает данные о кандидатах по указанному имени.
+    :param candidate_name: имя, по которому необходимо осуществить поиск.
+    :return: имя (имена) кандидата, совпадающие с запросом.
     """
-    pass
+    return [candidate for candidate in __candidates_data if
+            candidate_name.strip().lower() in candidate['name'].strip().lower()]
 
 
 def get_candidates_by_skill(skill_name):
     """
-
-    :param skill_name:
-    :return:
+    Загружает данные о кандидатах по запрашиваемому навыку.
+    :param skill_name: навык, по которому необходимо осуществить поиск.
+    :return: имя (имена) кандидата с соответствующим навыком.
     """
-    pass
+    candidates = []
+    for candidate in __candidates_data:
+        skills = candidate['skills'].lower().split(', ')
+        if skill_name.lower() in skills:
+            candidates.append(candidate)
+    return candidates
